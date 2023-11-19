64ba34b2716debea2f604bee40184e0ca6a99628
jamie-codez <cruiseomondi90@gmail.com>
2023-11-20 01:43:40 +0300

    Initial commit

---
+++ app/src/main/java/com/jamie/app/protologin/LoginScreen.kt
@@ -0,0 +1,251 @@
+package com.jamie.app.protologin
+
+import android.widget.CheckBox
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Email
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.Divider
+import androidx.compose.material3.DividerDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.jamie.app.protologin.ui.theme.AppBlackColor
+import com.jamie.app.protologin.ui.theme.AppBlueColor
+import com.jamie.app.protologin.ui.theme.AppFocusColor
+import com.jamie.app.protologin.ui.theme.AppUnFocusedColor
+import com.jamie.app.protologin.ui.theme.BackgroundColor
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Preview(showBackground = true)
+@Composable
+fun LoginScreen() {
+    var email by remember { mutableStateOf(TextFieldValue("")) }
+    var password by remember { mutableStateOf(TextFieldValue("")) }
+    var passwordVisible by rememberSaveable { mutableStateOf(false) }
+    var checkedState by remember { mutableStateOf(false) }
+    val visibilityIcon = if (passwordVisible) Icon(
+        painter = painterResource(id = R.drawable.visibility),
+        contentDescription = "Hide password"
+    ) else Icon(
+        painter = painterResource(id = R.drawable.visibility_off),
+        contentDescription = "Show Password"
+    )
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(BackgroundColor)
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.cirlce_ring),
+            contentDescription = "Login ring",
+            modifier = Modifier
+                .align(Alignment.TopEnd)
+                .size(100.dp)
+                .offset(x = 10.dp, y = (-5).dp)
+        )
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier
+                .fillMaxSize()
+                .verticalScroll(rememberScrollState())
+                .padding(horizontal = 24.dp, vertical = 32.dp)
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.login_logo),
+                contentDescription = "Login logo"
+            )
+            Text(
+                text = stringResource(id = R.string.welcome_back),
+                fontWeight = FontWeight.Bold,
+                fontFamily = FontFamily.Serif,
+                fontSize = 28.sp,
+                color = AppBlackColor
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+            Text(
+                text = stringResource(id = R.string.sign_in_continue),
+                fontWeight = FontWeight.Normal,
+                fontFamily = FontFamily.Serif,
+                fontSize = 16.sp,
+                color = AppBlackColor
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+            OutlinedTextField(
+                value = email,
+                onValueChange = { email = it },
+                label = { Text(text = stringResource(id = R.string.label_email_address)) },
+                placeholder = { Text(text = stringResource(id = R.string.label_email_address)) },
+                shape = RoundedCornerShape(8.dp),
+                colors = TextFieldDefaults.outlinedTextFieldColors(
+                    focusedBorderColor = AppBlackColor,
+                    unfocusedBorderColor = AppUnFocusedColor
+                ),
+                leadingIcon = {
+                    Icon(
+                        imageVector = Icons.Outlined.Email,
+                        contentDescription = "Email icon",
+                        tint = AppFocusColor
+                    )
+                },
+                modifier = Modifier.fillMaxWidth()
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+            OutlinedTextField(
+                value = password,
+                onValueChange = { password = it },
+                label = { Text(text = stringResource(id = R.string.hint_password)) },
+                placeholder = { Text(text = stringResource(id = R.string.hint_password)) },
+                shape = RoundedCornerShape(8.dp),
+                colors = TextFieldDefaults.outlinedTextFieldColors(
+                    focusedBorderColor = AppUnFocusedColor,
+                    unfocusedBorderColor = AppUnFocusedColor
+                ),
+                leadingIcon = {
+                    Icon(
+                        painter = painterResource(id = R.drawable.password),
+                        contentDescription = "Password icon",
+                        tint = AppFocusColor
+                    )
+                },
+                modifier = Modifier.fillMaxWidth(),
+                visualTransformation = if (passwordVisible) VisualTransformation.None
+                else PasswordVisualTransformation(),
+                trailingIcon = {
+                    IconButton(onClick = { passwordVisible = !passwordVisible }) {
+                        if (passwordVisible) Icon(
+                            painter = painterResource(id = R.drawable.visibility),
+                            contentDescription = "Hide password"
+                        ) else Icon(
+                            painter = painterResource(id = R.drawable.visibility_off),
+                            contentDescription = "Show Password"
+                        )
+                    }
+                }
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Row(verticalAlignment = Alignment.CenterVertically) {
+                    Checkbox(
+                        checked = checkedState,
+                        onCheckedChange = { checkedState = it }
+                    )
+                    Text(
+                        text = stringResource(id = R.string.remember_me),
+                        fontSize = 14.sp,
+                        color = AppFocusColor
+                    )
+                }
+                Spacer(modifier = Modifier.weight(1f))
+                Text(
+                    text = stringResource(id = R.string.forgot_password),
+                    fontSize = 14.sp,
+                    color = AppBlueColor,
+                    modifier = Modifier.clickable { }
+                )
+            }
+            Spacer(modifier = Modifier.height(8.dp))
+            Button(
+                onClick = { /*TODO*/ },
+                shape = RoundedCornerShape(30.dp),
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = AppBlueColor,
+                    disabledContainerColor = Color.Gray,
+                    disabledContentColor = Color(245)
+                ),
+                modifier = Modifier
+                    .width(300.dp)
+                    .height(50.dp)
+            ) {
+                Text(
+                    text = stringResource(id = R.string.sign_in).uppercase(),
+                    style = TextStyle(
+                        color = Color.White,
+                        fontSize = 14.sp,
+                        fontWeight = FontWeight.Bold
+                    )
+                )
+            }
+            Spacer(modifier = Modifier.height(10.dp))
+            Row(
+                horizontalArrangement = Arrangement.SpaceEvenly,
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Divider(
+                    modifier = Modifier.width(100.dp),
+                    thickness = 1.dp,
+                    color = AppBlackColor
+                )
+                Text(
+                    text = "OR",
+                    Modifier.padding(start = 10.dp, end = 10.dp)
+                )
+                Divider(
+                    modifier = Modifier.width(100.dp),
+                    thickness = 1.dp,
+                    color = AppBlackColor
+                )
+
+            }
+        }
+    }
+}
+
+//@Composable
+//fun Divider(
+//    modifier: Modifier = Modifier,
+//    thickness: Dp = DividerDefaults.Thickness,
+//    color: Color = DividerDefaults.color
+//) {
+//
+//}
